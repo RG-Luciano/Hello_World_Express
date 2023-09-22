74638dd6138bd932368743c995ad0c5fb51abe57
Luciano Ramos Garcia <lucianorgarcia2009@gmail.com>
2023-09-22 00:38:24 -0300

    talvez eu tenha conseguido

--- src/actions/authorization.ts
+++ src/actions/authorization.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 const secretKey = 'SecretKey';
 
 const loginUser = (req: Request, res: Response) => {
-    const { username, password } = req.body;
+    const { username, password } = req.query;
     console.log("Recebido: username =", username, "password =", password);
 
     const users = [
@@ -13,6 +13,10 @@ const loginUser = (req: Request, res: Response) => {
     ];
 
     const user = users.find(u => u.username === username && u.password === password);
+
+    if(!username || !password){
+        return res.status(400).json({message:'Field cannot be empty'})
+    }
 
     if (!user) {
         return res.status(401).json({ message: 'Incorret User or Password' });
